50e83f7e15ef6a8cec363f2f2602c4ea894cd4a6
Nicolas Delvaux <nicolas.delvaux@cji.paris>
2020-08-31 11:50:22 +0200

    Format the code with Black

--- importer/decoders.py
+++ importer/decoders.py
@@ -1,104 +1,117 @@
 # Decoders
 
+
 class Decoder(object):
 
     mime_type = None
 
     def __init__(self):
         self._obj = None
-        self._input = 'None'
+        self._input = "None"
 
     def decode(self, obj):
         self._obj = obj
 
     def __str__(self):
-        if self._obj: return self.decode(self._obj)
+        if self._obj:
+            return self.decode(self._obj)
         return "<Decoder %s>" % str(self._input)
+
 
 class DefaultDecoder(Decoder):
 
-    mime_type = 'text/plain'
+    mime_type = "text/plain"
 
     def __init__(self):
         super(DefaultDecoder, self).__init__()
-        self._input = 'default'
+        self._input = "default"
 
     def decode(self, obj):
         super(DefaultDecoder, self).decode(obj)
         return str(obj)
 
+
 class XmlDecoder(Decoder):
 
-    mime_type = 'text/xml'
+    mime_type = "text/xml"
 
     def __init__(self):
         super(XmlDecoder, self).__init__()
-        self._input = 'xml'
+        self._input = "xml"
 
     def decode(self, obj):
         super(XmlDecoder, self).decode(obj)
-        #TODO
+        # TODO
+
 
 class SoapDecoder(Decoder):
 
-    mime_type = 'application/soap+xml'
+    mime_type = "application/soap+xml"
 
     def __init__(self):
         super(SoapDecoder, self).__init__()
-        self._input = 'soap'
+        self._input = "soap"
 
     def decode(self, obj):
         super(SoapDecoder, self).decode(obj)
-        #TODO
+        # TODO
+
 
 class JSONDecoder(Decoder):
 
-    mime_type = 'application/json'
+    mime_type = "application/json"
 
     def __init__(self):
         super(JSONDecoder, self).__init__()
-        self._input = 'json'
+        self._input = "json"
 
     def decode(self, obj):
         super(JSONDecoder, self).decode(obj)
         import django.utils.simplejson as json
+
         return json.JSONDecoder().decode(obj)
+
 
 class PickleDecoder(Decoder):
 
-    mime_type = 'application/pickle'
+    mime_type = "application/pickle"
 
     def __init__(self):
         super(PickleDecoder, self).__init__()
-        self._input = 'pickle'
+        self._input = "pickle"
 
     def decode(self, obj):
         super(PickleDecoder, self).decode(obj)
         import pickle
+
         return pickle.loads(obj)
+
 
 class MsgpackDecoder(Decoder):
 
-    mime_type = 'application/x-msgpack'
+    mime_type = "application/x-msgpack"
 
     def __init__(self):
         super(MsgpackDecoder, self).__init__()
-        self._input = 'msgpack'
+        self._input = "msgpack"
 
     def decode(self, obj):
         super(MsgpackDecoder, self).decode(obj)
         import msgpack
+
         return msgpack.unpackb(obj, encoding="utf-8")
+
 
 class DecoderFactory(object):
     """ DecoderFactory handles Decoder instances for the right input mode. """
+
     _input = {
-        'default': DefaultDecoder,
-        'xml'    : XmlDecoder,
-        'soap'   : SoapDecoder,
-        'json'   : JSONDecoder,
-        'pickle' : PickleDecoder,
-        'msgpack': MsgpackDecoder,
+        "default": DefaultDecoder,
+        "xml": XmlDecoder,
+        "soap": SoapDecoder,
+        "json": JSONDecoder,
+        "pickle": PickleDecoder,
+        "msgpack": MsgpackDecoder,
     }
 
     @staticmethod
@@ -108,7 +121,7 @@ class DecoderFactory(object):
             If the input mode is invalid, returns a DefaultDecoder.
         """
 
-        decoder = 'default'
+        decoder = "default"
         if in_decoder in DecoderFactory._input:
             decoder = in_decoder
         return DecoderFactory._input[decoder]()
--- importer/generators.py
+++ importer/generators.py
@@ -1,110 +1,124 @@
 # Generator
 
+
 class Generator(object):
 
     mime_type = None
 
     def __init__(self):
         self._obj = None
-        self._output = 'None'
+        self._output = "None"
 
     def generate(self, obj):
         self._obj = obj
 
     def __str__(self):
-        if self._obj: return self.generate(self._obj)
+        if self._obj:
+            return self.generate(self._obj)
         return "<Generator %s>" % str(self._output)
+
 
 class XmlGenerator(Generator):
 
-    mime_type = 'text/xml'
+    mime_type = "text/xml"
 
     def __init__(self):
         super(XmlGenerator, self).__init__()
-        self._output = 'xml'
+        self._output = "xml"
 
     def generate(self, obj):
         super(XmlGenerator, self).generate(obj)
-        #TODO
+        # TODO
+
 
 class SoapGenerator(Generator):
 
-    mime_type = 'application/soap+xml'
+    mime_type = "application/soap+xml"
 
     def __init__(self):
         super(SoapGenerator, self).__init__()
-        self._output = 'soap'
+        self._output = "soap"
 
     def generate(self, obj):
         super(SoapGenerator, self).generate(obj)
-        #TODO
+        # TODO
+
 
 class JSONGenerator(Generator):
 
-    mime_type = 'application/json'
+    mime_type = "application/json"
 
     def __init__(self):
         super(JSONGenerator, self).__init__()
-        self._output = 'json'
+        self._output = "json"
 
     def generate(self, obj):
         super(JSONGenerator, self).generate(obj)
         import django.utils.simplejson as json
         from django.core.serializers.json import DateTimeAwareJSONEncoder
+
         return json.dumps(obj, cls=DateTimeAwareJSONEncoder)
+
 
 class PickleGenerator(Generator):
 
-    mime_type = 'application/pickle'
+    mime_type = "application/pickle"
 
     def __init__(self):
         super(PickleGenerator, self).__init__()
-        self._output = 'pickle'
+        self._output = "pickle"
 
     def generate(self, obj):
         super(PickleGenerator, self).generate(obj)
         import pickle
+
         try:
             return pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
         except pickle.PickleError as _error:
             # cPickle failed, try pickle
             import pickle
+
             return pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
+
 
 class MsgpackGenerator(Generator):
 
-    mime_type = 'application/x-msgpack'
+    mime_type = "application/x-msgpack"
 
     def __init__(self):
         super(MsgpackGenerator, self).__init__()
-        self._output = 'msgpack'
+        self._output = "msgpack"
 
     def generate(self, obj):
         super(MsgpackGenerator, self).generate(obj)
         import msgpack
+
         return msgpack.packb(obj, default=str)
+
 
 class DefaultGenerator(Generator):
 
-    mime_type = 'text/plain'
+    mime_type = "text/plain"
 
     def __init__(self):
         super(DefaultGenerator, self).__init__()
-        self._output = 'default'
+        self._output = "default"
 
     def generate(self, obj):
         super(DefaultGenerator, self).generate(obj)
         return str(obj)
 
+
 class GeneratorFactory(object):
     """ GeneratorFactory handles Generator instances for the right output mode. """
+
     _output = {
-        'xml'    : XmlGenerator,
-        'soap'   : SoapGenerator,
-        'json'   : JSONGenerator,
-        'pickle' : PickleGenerator,
-        'default': DefaultGenerator,
-        'msgpack': MsgpackGenerator,
+        "xml": XmlGenerator,
+        "soap": SoapGenerator,
+        "json": JSONGenerator,
+        "pickle": PickleGenerator,
+        "default": DefaultGenerator,
+        "msgpack": MsgpackGenerator,
     }
 
     @staticmethod
@@ -114,7 +128,7 @@ class GeneratorFactory(object):
             If the output mode is invalid, returns a DefaultGenerator.
         """
 
-        out = 'default'
+        out = "default"
         if output in GeneratorFactory._output:
             out = output
         return GeneratorFactory._output[out]()
